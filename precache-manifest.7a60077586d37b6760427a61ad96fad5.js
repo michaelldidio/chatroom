d0ea762a1a92dab87c781a6c4d68bb7c06776aec
michaelldidio <michaelldidio@gmail.com>
2022-07-26 16:57:34 -0400

    Updates

---
+++ precache-manifest.7a60077586d37b6760427a61ad96fad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5767934e8d458af633235af677a3e6e",
+    "url": "/chatroom/index.html"
+  },
+  {
+    "revision": "2bc1cc540944108cc3d3",
+    "url": "/chatroom/static/css/main.a82975ec.chunk.css"
+  },
+  {
+    "revision": "a19eba21574c37eab7d0",
+    "url": "/chatroom/static/js/2.9b208412.chunk.js"
+  },
+  {
+    "revision": "2bc1cc540944108cc3d3",
+    "url": "/chatroom/static/js/main.04acceac.chunk.js"
+  },
+  {
+    "revision": "6583d80d61c57cf6497c",
+    "url": "/chatroom/static/js/runtime~main.65695709.js"
+  }
+]);
